da7ac607e77089822993b885423432433308a205
Thomas <thwr@itu.dk>
2021-09-04 09:24:15 +0200

    implement HelloWorld

--- lecture/Program.cs
+++ lecture/Program.cs
@@ -2,11 +2,11 @@
 
 namespace lecture
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
