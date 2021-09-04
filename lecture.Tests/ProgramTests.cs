7ad41dd1ee8dd999f9ec6910be64e88cc0a9d8b9
ThomasITU <75098556+ThomasITU@users.noreply.github.com>
2021-09-04 12:34:40 +0200

    Implement IsLeapYear (#3)
    * Created user interface for IsLeapYear
    * Implemented error handling

--- lecture.Tests/ProgramTests.cs
+++ lecture.Tests/ProgramTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Windows;
 using Xunit;
 
 
@@ -10,23 +11,20 @@ namespace lecture.Tests
         [Fact]
         public void Main_Prints_Hello_World()
         {   
-            //arrange
             var writer = new StringWriter();
             Console.SetOut(writer);
 
-            //act
             Program.Main(new string[0]);
             var actual = writer.GetStringBuilder().ToString().Trim();
 
-            //assert
             Assert.Equal("Hello, World!", actual);
         }
 
         [Fact]
         public void IsLeapYear_Abides_The_4Year_Rule()
         {
-            bool ActualIsLeapYear = Program.IsLeapYear(4);
-            bool ActualIsNotLeapYear = Program.IsLeapYear(77);
+            bool ActualIsLeapYear = Program.IsLeapYear(1596);
+            bool ActualIsNotLeapYear = Program.IsLeapYear(1597);
         
             Assert.True(ActualIsLeapYear);
             Assert.False(ActualIsNotLeapYear);
@@ -47,6 +45,54 @@ namespace lecture.Tests
         
             Assert.True(ActualIsLeapYear);
         }
+
+        [Fact]
+        public void IsLeapYearPromt_prints_yay()
+        {
+            /*
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main(new string[0]);
+            Console.WriteLine(4);
+            SendKeys.Send("{ENTER}");
+            var actual = writer.GetStringBuilder().ToString().Trim();
+
+            Assert.Equal("yay", actual);
+            */
+
+            string actualString = Program.IsLeapYearPromt(2000);
+
+            Assert.Equal("yay", actualString);
+        }
+
+        [Fact]
+        public void IsLeapYearPromt_prints_nay()
+        {
+            string actual = Program.IsLeapYearPromt(1900);
+
+            Assert.Equal("nay", actual);
+        }
+
+        [Fact]
+        public void IsLeapYear_does_not_apply_before_1582()
+        {
+            bool actual = Program.IsLeapYear(1580);
+            
+            Assert.False(actual);
+        }
+        [Fact]
+        public void IsLeapYearPromt_handle_StringInput()
+        {
+            // same problem as further above, do not know how to handle the console input yet
+            /*
+            
+            string actual = Program.IsLeapYearPromt("asdasd");
+
+            Assert.Equal("The given input is not a year, try something like \"2000\"", actual);
+            */
+        }
+        
 
     }
 }
--- lecture/Program.cs
+++ lecture/Program.cs
@@ -6,14 +6,37 @@ namespace lecture
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!\n\n");
+            
+
+                  
+            // Commented because it breaks tests by freezing terminal because missing console input -- Deadlock?
+            /*
+            Console.WriteLine("Type in a year and hit enter:\n");
+            try
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(IsLeapYearPromt(input));                 
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("The given input is not a year, try something like \"2000\"");
+            }
+            */
+            
         }
       
         public static bool IsLeapYear(int year)
         {
-            if(year % 400 == 0) return true;
+            if (year < 1582) return false;
+            else if (year % 400 == 0) return true;
             else if(year % 100 == 0) return false;
             return year % 4 == 0 ? true : false;
+        }
+
+        public static string IsLeapYearPromt(int year)
+        {
+            return IsLeapYear(year) ? "yay" : "nay";
         }
     }
 }
